9f009673de207aa575df931e92a3a36994ef0cbc
filipe <fda051@gmail.com>
2022-08-12 18:57:15 -0300

    primeiro commit javascript

---
+++ alura.js
@@ -0,0 +1,4 @@
+const text = 'Hello World NodeJS!!!!!'
+const nome = 'Filipe'
+
+console.log(text + ' ' + '\n' + nome)
---
+++ type_boolean.js
@@ -0,0 +1,5 @@
+const text1 = 'OLA'
+const text2 = 'HOLA'
+
+
+console.log(text1 == text2);
